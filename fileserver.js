af5c828a6e97cfb3d523138786ce1fa0342eb8b6
Adam Rashid <rashid.uj@gmail.com>
2020-09-30 21:03:25 +0800

    Added method.GET

--- fileserver.js
+++ fileserver.js
@@ -30,22 +30,64 @@ let server = createServer((request, response) => {
     });
 }).listen(8000);
 
-const { parse } = require("url");
-const { resolve, sep } = require("path");
-
-const baseDirectory = process.cwd();
-function urlPath(url) {
-  let { pathname } = parse(url);
-  let path = resolve(decodeURIComponent(pathname).slice(1));
-  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
-    throw { status: 403, body: "Forbidden" };
-  }
-  return path;
-}
-
 async function notAllowed(request) {
   return {
     status: 405,
     body: `Method ${request.method} is not allowed`,
   };
 }
+
+const { parse } = require("url");
+const { resolve, sep } = require("path");
+
+// Returns the current working directory of the process
+const baseDirectory = process.cwd();
+
+function urlPath(url) {
+  // Destructuring syntax, assigns the result of parse.pathname to an object called pathname
+  // Parses the url to each component, then only gets the specific pathname
+  // TODO: Change to WHATWG
+  let { pathname } = parse(url);
+
+  // URL returned from parse is percent-encoded (e.g. ? is %3F) and it returns the / in front, and thus must be sliced.
+  // The resolve function returns the absolute path requested, meaning the path that includes the baseDirectory plus the url's path
+  let path = resolve(decodeURIComponent(pathname).slice(1));
+
+  // Checking if the resolved path is the base directory or an extension of it. If not, throw an error
+  // sep provides the platform-specific path segment separator
+  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
+    throw { status: 403, body: "Forbidden" };
+  }
+
+  // If yes, return the path of the file
+  return path;
+}
+
+const { createReadStream } = require("fs");
+// Promises interface version of fs.stat() and fs.readdir(), to be able to use in async functions
+const { stat, readdir } = require("fs").promises;
+// MIME-type handler library, which can automatically get the MIME type for any particular file type
+const mime = require("mime");
+
+methods.GET = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+
+  // Get's some metadata on the file that is located at path. If found, assign to stats. If not found, throw an error.
+  try {
+    // TODO: Replace stat() call here with just. Not recommended according to the Node.js documentation
+    // https://nodejs.org/api/fs.html#fs_fs_stat_path_options_callback
+    stats = await stat(path);
+  } catch (error) {
+    // ENOENT is an error code when file is not found
+    if (error.code != "ENOENT") throw error;
+    else return { status: 404, body: "File not found" };
+  }
+
+  // Checks if the path of the item that is a directory. If yes, get all files in the directory
+  if (stats.isDirectory()) {
+    return { body: (await readdir(path)).join("\n") };
+  } else {
+    return { body: createReadStream(path), type: mime.getType(path) };
+  }
+};
